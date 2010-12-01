85dbd65d58a23601d38177285417446a0bfd6f91
Patrick Engel <zenobic+github@gmail.com>
2010-12-01 04:51:40 +0100

    added Docblocks

--- Config_Lite/Lite/InvalidArgumentException.php
+++ Config_Lite/Lite/InvalidArgumentException.php
@@ -1,3 +1,35 @@
 <?php
+/**
+ * Config_Lite_InvalidArgumentException (Config/Lite/InvalidArgumentException.php)
+ *
+ * PHP version 5
+ *
+ * @file      Config/Lite/InvalidArgumentException.php
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <info@pc-e.org>
+ * @copyright 2010 info@pc-e.org
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   Release: @package_version@
+ * @link      https://github.com/pce/config_lite
+ */
 
-class Config_Lite_InvalidArgumentException extends InvalidArgumentException implements Config_Lite_Exception {}
+
+/**
+ * Config_Lite_InvalidArgumentException
+ *
+ * implements Config_Lite_Exception
+ *
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <info@pc-e.org>
+ * @copyright 2010 info@pc-e.org
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   Release: @package_version@
+ * @link      https://github.com/pce/config_lite
+ */
+class Config_Lite_InvalidArgumentException
+              extends RuntimeException
+              implements Config_Lite_Exception
+{
+}
